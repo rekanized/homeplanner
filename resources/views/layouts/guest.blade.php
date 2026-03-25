5303a8fdc0d50c370f0bcadf5f3f3c6b7e0719d2
Magnus Schöllin <magnus.schollin@gmail.com>
2026-03-25 13:43:47 +0100

    refactor: extract initial setup page styling from Blade template to a new `auth.css` file.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&family=Outfit:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
         @livewireStyles
     </head>
     <body class="antialiased">
